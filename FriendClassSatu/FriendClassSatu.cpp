4f64ca16a7768262e02df1c4adcd70c93017e908
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-07 18:19:50 +0700

    class orang

---
+++ FriendClassSatu/FriendClassSatu.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+int main()
+{
+    
+}
