fe5eb1d4d0010fa5dfbd09665299ed5c1337ae4b
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-07 18:28:23 +0700

    penambahan fungsi void siswa menampilkan nama

--- FriendClassSatu/FriendClassSatu.cpp
+++ FriendClassSatu/FriendClassSatu.cpp
@@ -20,8 +20,13 @@ private:
 
 public:
     void setId(int pId);
-    void displayAll(orang& a);
+    void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
 
 
 
