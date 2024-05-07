5049f1a1918fef4a803fa2810e39e351d3e3bf63
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-07 18:53:36 +0700

    fungsi int main

--- FriendFunctionDua/FriendFunctionDua.cpp
+++ FriendFunctionDua/FriendFunctionDua.cpp
@@ -25,5 +25,8 @@ void manusia::showNilaiPelajar(pelajar &x)
 }
 int main()
 {
-    std::cout << "Hello World!\n";
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
 }
